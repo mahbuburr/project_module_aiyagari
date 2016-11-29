be1536eaa78f39d9b86da3c7980f3983b79a84d7
Andrian Yambolov <a.yambolov@gmail.com>
2016-11-29 14:23:18 +0100

    changes in k_current

--- krussel_smith.m
+++ krussel_smith.m
@@ -1,9 +1,8 @@
 clear all
 close all
-clc
 
 parameters; % load parameters
-parameters_krussel_smith; % load parameters for Krussel - Smith
+% parameters_krussel_smith; % load parameters for Krussel - Smith
 
 [id_shock,ag_shock]  = generate_shocks(prob,T,ind_no,U_b); % generate shocks
 
@@ -12,8 +11,23 @@ parameters_krussel_smith; % load parameters for Krussel - Smith
 K_lims = [K_rep_b,grid_k(end)]; % initial limits for bisection method
 
 % initial guesses
-k_guess = mat_k; % policy function of agents (1st column unemployed, 2nd column employed)
+% k_guess = mat_k; % policy function of agents (1st column unemployed, 2nd column employed)
+% k_guess = repmat(0.9*37.98,[100,4,2,2]);
 K_guess = (K_lims(1)+K_lims(2))/2;
+
+k_guess=zeros(100,4,2,2); % next-period individual 
+   % capital (k') depends on four state variables: individual k, aggregate k, 
+   % aggregate shock, idiosyncratic shock 
+
+% Initial capital function
+
+for i=1:4
+   for j=1:2
+      for h=1:2
+         k_guess(:,i,j,h)=0.9*grid_k;
+      end
+   end
+end
 
 dif_B = 1;
 iter = 0;
@@ -25,7 +39,7 @@ while dif_B>1e-6 && iter<50 % loop for aggregate problem
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     disp('Solving individual problem');
     d2 = 1;
-    while d2>1e-10 % loop for household problem
+    while d2>1e-8 % loop for household problem
         kmprime=zeros(grid_k_no,ngridkm,2,2);
         kmprime(:,:,1,1)=exp(B(1)*ones(grid_k_no,ngridkm)+B(2)*log(kmaux(:,:,1,1)));
         kmprime(:,:,1,2)=exp(B(1)*ones(grid_k_no,ngridkm)+B(2)*log(kmaux(:,:,1,2)));
@@ -53,46 +67,68 @@ while dif_B>1e-6 && iter<50 % loop for aggregate problem
 %             end
 %         end
 
-
+   % Bad aggregate state and unemployed idiosyncratic state 
+   
+     k_next_bu=interpn(grid_k,km,k_guess(:,:,1,1),k_guess,kmprime,'cubic'); 
+        % finding the individual policy function k''=k(k',km') by interpolating
+        % the previously found policy function k'=k(k,km) in new points (k',km')
+%      cprime_bu=irate_b.*kprime+mu*(wage_b.*ones4)+(1-delta)*kprime-k_next_bu; 
+                                                  % future consumption (c')
+    
+     c_next_bu=max(1e-10,(1+r_mat(km,L(1),z(1))-delta).*k_guess+mu*w_mat(km,L(1),z(1))-k_next_bu);                                            
+     mu_next_bu=muc_inv(c_next_bu); % marginal utility of future consumption
+   
+   % Bad aggregate state and employed idiosyncratic state
+     ones4 = ones(100,4,2,2);
+     k_next_be=interpn(grid_k,km,k_guess(:,:,1,2),k_guess,kmprime,'cubic');
+     c_next_be=max(1e-10,(1-delta+r_mat(km,L(1),z(1))).*k_guess+w_mat(km,L(1),z(1)).*((1*l_bar-mu*((U_b./(L_b))))*ones4) - k_next_be);
+     mu_next_be=muc_inv(c_next_be);
+ 
+   % Good aggregate state and unemployed idiosyncratic state
+   
+     k_next_gu=interpn(grid_k,km,k_guess(:,:,2,1),k_guess,kmprime,'cubic');
+     c_next_gu=max(1e-10,(1+r_mat(km,L(2),z(2))-delta).*k_guess+mu*w_mat(km,L(2),z(2))-k_next_gu);
+     mu_next_gu=muc_inv(c_next_gu);
+   
+   % Good aggregate state and employed idiosyncratic state
+   
+     k_next_ge=interpn(grid_k,km,k_guess(:,:,2,2),k_guess,kmprime,'cubic');
+     c_next_ge=max(1e-10,(1-delta+r_mat(km,L(2),z(2))).*k_guess+w_mat(km,L(2),z(2)).*((1*l_bar-mu*((U_g./(L_g))))*ones4) - k_next_ge);
+     mu_next_ge=muc_inv(c_next_ge);
         
         
 %       calculate expected marginal utility of consumption next period
-        Emuc_next(:,1,1) = prob(1,1)*muc(c_next(1,:,1,1,1)) + prob(1,2)*muc(c_next(1,:,2,1,1)) + ... % currently unemployed in bad state
-                           prob(1,3)*muc(c_next(1,:,1,1,2)) + prob(1,4)*muc(c_next(1,:,2,1,2));
-                       
-        Emuc_next(:,2,1) = prob(2,1)*muc(c_next(2,:,1,1,1)) + prob(2,2)*muc(c_next(2,:,2,1,1)) + ... % currently employed in bad state
-                           prob(2,3)*muc(c_next(2,:,1,1,2)) + prob(2,4)*muc(c_next(2,:,2,1,2));    
-                     
-        Emuc_next(:,1,2) = prob(3,1)*muc(c_next(1,:,1,2,1)) + prob(3,2)*muc(c_next(1,:,2,2,1)) + ... % currently unemployed in good state
-                           prob(3,3)*muc(c_next(1,:,1,2,2)) + prob(3,4)*muc(c_next(1,:,2,2,2));
-                      
-	    Emuc_next(:,2,2) = prob(4,1)*muc(c_next(2,:,1,2,1)) + prob(4,2)*muc(c_next(2,:,2,2,1)) + ... % currently employed in good state
-                           prob(4,3)*muc(c_next(2,:,1,2,2)) + prob(4,4)*muc(c_next(2,:,2,2,2));
-                      
-       for i=1:2
-           for n=1:2
-               for m=1:2
-                   % calculate implied consumption this period from Euler equation
-                   c_current(:,i,n) = muc_inv(beta*(1+r(K_guess,L(m),z(m))-delta)*Emuc_next(:,i,n));
-                   % income matrix
-                   income = mat_income(K_guess,L(n),z(n));
-                   
-                   % calculate implied capital demand from budget constraint
-                   k_new(:,i,n) = (1+r(K_guess,L(n),z(n))-delta)*grid_k' + income(:,n) - c_current(:,i,n);
-               end
-           end
-       end
+     Emuc_next =(mu_next_bu.*(1-delta+r_mat(km,L(1),z(1)))).*prob_bu+...
+     (mu_next_be.*(1-delta+r_mat(km,L(1),z(1)))).*prob_be+...
+     (mu_next_gu.*(1-delta+r_mat(km,L(1),z(1)))).*prob_gu+...
+     (mu_next_ge.*(1-delta+r_mat(km,L(1),z(1)))).*prob_ge;
+     
+     c_current = muc_inv(beta*Emuc_next);
+     k_current = NaN(100,4,2,2);
+     raux = (1+r_mat(km,L(1),z(1))-delta);
+     waux = w_mat(km,L(1),z(1));
+     k_current(:,:,1,1) = raux(:,:,1,1).*grid_k+mu*waux(:,:,1,1)-c_current(:,:,1,1);
+     k_current(:,:,1,2) = raux(:,:,1,2).*grid_k+waux(:,:,1,2).*((2*l_bar-mu*((U_b./(L_b))))*ones(100,4)) - c_current(:,:,1,2);
+     k_current(:,:,2,1) = raux(:,:,2,1).*grid_k+mu*waux(:,:,2,1)-c_current(:,:,2,1);
+     k_current(:,:,2,2) = raux(:,:,2,2).*grid_k+waux(:,:,2,2).*((2*l_bar-mu*((U_g./(L_g))))*ones(100,4)) - c_current(:,:,2,2);
+     k_current=(k_current>=grid_k(1)).*(k_current<=grid_k(end)).*k_current+(k_current<grid_k(1))*k_min+(k_current>grid_k(end))*grid_k(end);
                        
        
         % apply borrowing constraint to get new policy function
-        k_new = max(k_min*K_guess,k_new); 
-        
-        d2_1 = norm(abs(k_new(:,1,1)-k_guess(:,1,1))./(1+abs(k_guess(:,1,1)))); % deviation between guess and new policy function
-        d2_2 = norm(abs(k_new(:,2,1)-k_guess(:,2,1))./(1+abs(k_guess(:,2,1))));
-        d2_3 = norm(abs(k_new(:,1,2)-k_guess(:,1,2))./(1+abs(k_guess(:,1,2))));
-        d2_4 = norm(abs(k_new(:,2,2)-k_guess(:,2,2))./(1+abs(k_guess(:,2,2))));
-        d2 = max([d2_1, d2_2, d2_3, d2_4]);
-        
+%         k_new = max(k_min*K_guess,k_new); 
+        d2=max(max(max(max(abs(k_new-k_guess)))));
+%         d2_1 = norm(abs(k_new(:,1,1)-k_guess(:,1,1))./(1+abs(k_guess(:,1,1)))); % deviation between guess and new policy function
+%         d2_2 = norm(abs(k_new(:,2,1)-k_guess(:,2,1))./(1+abs(k_guess(:,2,1))));
+%         d2_3 = norm(abs(k_new(:,1,2)-k_guess(:,1,2))./(1+abs(k_guess(:,1,2))));
+%         d2_4 = norm(abs(k_new(:,2,2)-k_guess(:,2,2))./(1+abs(k_guess(:,2,2))));
+%         d2 = max([d2_1, d2_2, d2_3, d2_4]);
+r_b = r_mat(km,L(1),z(1));
+r_g = r_mat(km,L(2),z(2));
+w_b = w_mat(km,L(1),z(1));
+w_g = w_mat(km,L(2),z(2));
+        save('some_results.mat', 'grid_k' ,'km','k_guess','kmprime', 'c_next_bu', 'mu_next_bu', 'k_next_bu', ...
+            'c_next_be', 'mu_next_be', 'k_next_be', 'c_next_gu', 'mu_next_gu', 'k_next_gu', 'c_next_ge', 'mu_next_ge', 'k_next_ge', ...
+            'Emuc_next', 'c_current', 'k_new', 'r_b', 'r_g', 'w_b', 'w_g');
         % update policy function
         k_guess = k_guess + 0.5*(k_new-k_guess);
     end
--- parameters.m
+++ parameters.m
@@ -1,15 +1,15 @@
 %% set parameters
 % consumers
 beta = 0.99; % discount factor
-sigma = 2; % risk aversion
-mu = 0.3; % replacement rate of unemployed
-k_min = 0.5; % borrowing constraint as share of capital stock
+sigma = 1; % risk aversion
+mu = 0.15; % replacement rate of unemployed
+k_min = 10e-10; % borrowing constraint as share of capital stock
 ind_no = 5000; % number of individuals simulated
 T = 3000; % number of periods simulated
 
 % firms (production function F(K,L)=z*K^alpha*L^(1-alpha)
 delta = 0.025; % depreciation rate
-alpha = 0.33; % output elasticity of capital
+alpha = 0.36; % output elasticity of capital
 delta_a=0.01;    % (1-delta_a) is the productivity level in a bad state, 
                  % and (1+delta_a) is the productivity level in a good state
 z_g = 1+delta_a; % productivity in good aggreagate state
@@ -26,6 +26,7 @@ L_b=(1-U_b);   % employment rate in a bad aggregate state
 U_g=0.04;       % unemployment rate in a good aggregate state
 L_g=(1-U_g);   % employment rate in a good aggregate state
 L = [L_b; L_g]; % vector of states
+l_bar=1/0.9;
 
 %% setup
 % build transition matrix of agents
@@ -36,28 +37,41 @@ L = [L_b; L_g]; % vector of states
 % define some useful functions
 muc = @(c) c.^(-sigma); % marginal utility of consumption
 muc_inv = @(muc) muc.^(-1/sigma); % inverse of marginal utility of consumption
-w = @(K,L,z) z.*(1-alpha).*K.^alpha.*L.^(-alpha); % wage
-r = @(K,L,z) z.*alpha.*K.^(alpha-1).*L.^(1-alpha); % rental rate of capital
+w = @(K,L,z) z.*(1-alpha).*K.^alpha.*(L*l_bar).^(-alpha); % wage
+r = @(K,L,z) z.*alpha.*K.^(alpha-1).*(L*l_bar).^(1-alpha); % rental rate of capital
 K = @(L,r,z) L*(z*alpha/r).^(1/(1-alpha)); % capital stock necessary to yield return r
 Y = @(K,L,z) z*K.^alpha*L^(1-alpha); % output
 C = @(K) Y(K,L,z)-delta*K; % consumption
 tau = @(L) mu*(1-L)/L; % tax rate
+
+r_mat = @(K,L,z) repmat(r(K',L,z),100,1,2,2);
+w_mat = @(K,L,z) repmat(w(K',L,z),100,1,2,2);
 
 % define grid for individual capital on which to solve
 K_rep_g = K(L_g,1/beta-1+delta, z_g);% capital stock of representative agent useful for comparison good aggregate state
 K_rep_b = K(L_b,1/beta-1+delta, z_b);
 
 grid_k_no = 100; % number of grid points for agents' capital holdings
-grid_k = linspace(k_min*K_rep_b,3*K_rep_g,grid_k_no); % grid for agents' policy function
+% grid_k = linspace(k_min*K_rep_b,3*K_rep_g,grid_k_no); % grid for agents' policy function
+
+k_min=0;                   % minimum grid-value of capital
+k_max=1000;                % maximum grid-value of capital                % number of grid points
+x=linspace(0,0.5,grid_k_no)'; % generate a grid of ngridk points on [0,0.5] 
+                           % interval  
+y=x.^7/max(x.^7);          % polynomial distribution of grid points, formula 
+                           % (7) in the paper
+grid_k=k_min+(k_max-k_min)*y;   % transformation of grid points from [0,0.5] 
+                           % interval to [k_min,k_max] interval
 
 grid_dist_no = 1000;
 grid_dist = linspace(grid_k(1),grid_k(end),grid_dist_no); % grid for distribution of agents - use finer grid
 
 % Grid for mean of capital
 
-km_min=0.75*K_rep_b;                           % minimum grid-value of the mean of 
-                                     % capital distribution, km 
-km_max=1.25*K_rep_g;                           % maximum grid value of km
+% km_min=0.75*K_rep_b;                           % minimum grid-value of the mean of 
+km_min = 30;        % capital distribution, km 
+km_max = 50;
+% km_max=1.25*K_rep_g;                           % maximum grid value of km
 ngridkm=4;                           % number of grid points for km 
 km=linspace(km_min,km_max,ngridkm)'; % generate a grid of ngridkm points on 
                                      % [km_min,km_max] interval 
@@ -82,3 +96,44 @@ kmprime(:,:,1,2)=exp(B(1)*ones(grid_k_no,ngridkm)+B(2)*log(kmaux(:,:,1,2)));
 kmprime(:,:,2,1)=exp(B(3)*ones(grid_k_no,ngridkm)+B(4)*log(kmaux(:,:,2,1)));
 kmprime(:,:,2,2)=exp(B(3)*ones(grid_k_no,ngridkm)+B(4)*log(kmaux(:,:,2,2)));
 kmprime=(kmprime>=km_min).*(kmprime<=km_max).*kmprime+(kmprime<km_min)*km_min+(kmprime>km_max)*km_max; % restricting km' to be in [km_min,km_max] range
+
+nstates_ag = 2;
+nstates_id = 2;
+prob_bu=zeros(grid_k_no,4,nstates_ag,nstates_id); % for a bad agg. state 
+                           % and unemployed idios. state in the next period
+prob_be=zeros(grid_k_no,ngridkm,nstates_ag,nstates_id); % for a bad agg. state 
+                           % and employed idios. state in the next period
+prob_gu=zeros(grid_k_no,ngridkm,nstates_ag,nstates_id); % for a good agg. state 
+                           % and unemployed idios. state in the next period
+prob_ge=zeros(grid_k_no,ngridkm,nstates_ag,nstates_id); % for a good agg. state 
+                           % and employed idios. state in the next period
+
+%% Parameters for Krussel - Smith
+
+% Matrix of transition probabilities in Den Haan, Judd, Juillard (2008)
+
+prob=[0.525 0.35 0.03125 0.09375  
+   0.038889 0.836111 0.002083 0.122917
+   0.09375 0.03125 0.291667 0.583333
+   0.009115 0.115885 0.024306 0.850694];
+                         
+                           
+prob_bu(:,:,1,1)=prob(1,1)*ones(grid_k_no,ngridkm);
+prob_bu(:,:,1,2)=prob(2,1)*ones(grid_k_no,ngridkm);
+prob_bu(:,:,2,1)=prob(3,1)*ones(grid_k_no,ngridkm);
+prob_bu(:,:,2,2)=prob(4,1)*ones(grid_k_no,ngridkm);
+
+prob_be(:,:,1,1)=prob(1,2)*ones(grid_k_no,ngridkm);
+prob_be(:,:,1,2)=prob(2,2)*ones(grid_k_no,ngridkm);
+prob_be(:,:,2,1)=prob(3,2)*ones(grid_k_no,ngridkm);
+prob_be(:,:,2,2)=prob(4,2)*ones(grid_k_no,ngridkm);
+
+prob_gu(:,:,1,1)=prob(1,3)*ones(grid_k_no,ngridkm);
+prob_gu(:,:,1,2)=prob(2,3)*ones(grid_k_no,ngridkm);
+prob_gu(:,:,2,1)=prob(3,3)*ones(grid_k_no,ngridkm);
+prob_gu(:,:,2,2)=prob(4,3)*ones(grid_k_no,ngridkm);
+
+prob_ge(:,:,1,1)=prob(1,4)*ones(grid_k_no,ngridkm);
+prob_ge(:,:,1,2)=prob(2,4)*ones(grid_k_no,ngridkm);
+prob_ge(:,:,2,1)=prob(3,4)*ones(grid_k_no,ngridkm);
+prob_ge(:,:,2,2)=prob(4,4)*ones(grid_k_no,ngridkm);
