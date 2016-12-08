1ec348b521916991c2d4c995686f60ed61393b32
Andrian Yambolov <a.yambolov@gmail.com>
2016-12-08 02:19:26 +0100

    Bug found. K_grid instead of K_guess

---
+++ iseq.m
@@ -0,0 +1,9 @@
+isequal(k_next_bu, a.k2prime_bu)
+isequal(k_next_be, a.k2prime_be)
+isequal(k_next_gu, a.k2prime_gu)
+isequal(k_next_ge, a.k2prime_ge)
+
+isequal(c_next_bu, a.cprime_bu)
+isequal(c_next_be, a.cprime_be)
+isequal(c_next_gu, a.cprime_gu)
+isequal(c_next_ge, a.cprime_ge)
