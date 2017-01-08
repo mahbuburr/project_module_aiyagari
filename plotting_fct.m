d4aa31cfb5ac92a402d3489a2c1d2011063f2d31
Eric Lustenberger <eric.lustenberger@hotmail.com>
2017-01-08 14:56:55 +0100

    Added option to perform parameter analysis for different parameters. Up to date with parameters.m and setup.m format.

--- plotting_fct.m
+++ plotting_fct.m
@@ -1,26 +1,36 @@
-%% Analysis of parameters 
+%% Parameter Analysis
 clear 
 close 
 
-%% Specify the parameter of interest
-% 'Borrowing Constraint' 
-% 'Unemployment insurance'
-% 'Chance to get employed'
-% etc...
+parameters % import parameters 
 
-%study_parameter = 'Borrowing Constraint'
+%% Specify the parameter of interest and their values
+analysis = 'Risk Aversion'; % specify parameter you want to analyze: 'Borrowing Constraint' for min savings, 'Unemployment Insurance', 'Chance to be Employed','Risk Aversion'
+vals = [1,2,3];
+method.sim = 'histogram';
 
-k_min = [0,0.5,0.9];
+%% Create Figures for different values 
+% Initiating matrix to store aggregate values    
 T_mat = zeros(6,3);
-for i =1:3
-    parameters % import parameters 
-    method.sim = 'histogram' 
-    par.k_min = k_min(i);
-    
+
+for i =1:3 
+% loop over different values of parameter you want to analyze 
+    if strcmp(analysis,'Borrowing Constraint') 
+        par.k_min = vals(i);
+    elseif strcmp(analysis, 'Chance to be Employed')
+        par.PI_UE = vals(i);
+    elseif strcmp(analysis, 'Unemployment Insurance')
+        par.mu = vals(i);
+    elseif strcmp(analysis, 'Risk Aversion')
+        par.sigma = vals(i); 
+    end
+% importing functions and parameters that adapt to new input parameter value             
+    setup 
 % calling the problem solving function 
-    [k,c,K,sim,store]= aiyagari_solver(par, grid, K, k, func, method, mat); 
+    [k, c, K, sim, store, mat, grid] = aiyagari_solver(par, func, method); 
 
 %% Plot distribution of agents
+% create subplot for each iteration
     figure(1)
     subplot(1,3,i)
     if strcmp(method.sim,'simulation')
@@ -31,10 +41,18 @@ for i =1:3
         bar(grid.dist,store.distribution,'stacked')
         legend('unemployed','employed')
     end
-    title(['kmin = ',num2str(k_min(i))]);
- 
-%% Saving values of Aggregates 
-% calculating aggregates 
+% create title matching parameter in question
+    if strcmp(analysis,'Borrowing Constraint') 
+        title(['kmin = ',num2str(par.k_min)]);
+    elseif strcmp(analysis,'Chance to be Employed')
+        title(['PI_UE = ',num2str(par.PI_UE)]);
+    elseif strcmp(analysis,'Unemployment Insurance')
+        title(['mu = ',num2str(par.mu)]);
+    elseif strcmp(analysis,'Risk Aversion')
+        title(['sigma = ',num2str(par.sigma)]);
+    end
+    
+%% Calculating Aggregates 
     ag1 = K.guess;
     ag2 = func.Y(K.guess);
     ag3 = func.C(K.guess);
@@ -43,20 +61,30 @@ for i =1:3
     dev3= log(func.C(K.guess)/func.C(K.rep));   
     
 % saving aggregates in matrix
-T_mat(:,i) = [ag1 ag2 ag3 dev1 dev2 dev3];
-    
-end
+T_mat(:,i) = [ag1 ag2 ag3 dev1 dev2 dev3]; 
+end 
 
-% save figure as pdf
+%% Save figure as pdf 
 saveas(gcf,'kmin.pdf')
 
-value = {'Capital';'Output';'Consumption'};
+%% Create Tables for Aggregates 
 
-% Create Tables 
-T1 = table(T_mat(1:3,1),T_mat(4:end,1),'RowNames',value); 
-T2 = table(T_mat(1:3,2),T_mat(4:end,2),'RowNames',value);
-T3 = table(T_mat(1:3,3),T_mat(4:end,3),'RowNames',value);
+% set values for table
+Variable = {'Capital';'Output';'Consumption'};
+values = {'Aggregate variables';'log-deviation'};
+Aggregates = T_mat(1:3,1);
+log_deviations = T_mat(4:end,1);
 
-% % Combine Tables
-% 
-% T_final = join(T1,T2,T3);
+% create tables and save them
+T1 = table(Variable, Aggregates, log_deviations); 
+writetable(T1,'Table1.txt','Delimiter',' ')
+
+Aggregates = T_mat(1:3,2);
+log_deviations = T_mat(4:end,2);
+T2 = table(Variable, Aggregates, log_deviations);
+writetable(T2,'Table1.txt','Delimiter',' ')
+
+Aggregates = T_mat(1:3,3);
+log_deviations = T_mat(4:end,3);
+T3 = table(Variable, Aggregates, log_deviations);
+writetable(T3,'Table1.txt','Delimiter',' ')
---
+++ setup.m
@@ -0,0 +1,20 @@
+%% setup
+par.PI_EU = par.PI_UE*(1-par.L_target)/par.L_target; % chance of getting unemployed
+
+% build transition matrix of agents
+par.PI = [1-par.PI_UE,par.PI_UE;par.PI_EU,1-par.PI_EU]; % [UU,UE;EU;EE]
+par.L = par.PI(1,2)/(par.PI(1,2)+par.PI(2,1)); % calculate steady state supply of labour
+par.tau = par.mu*(1-par.L)/par.L; % tax rate
+
+% define some useful functions
+func.muc = @(c) c.^(-par.sigma); % marginal utility of consumption
+func.muc_inv = @(muc) muc.^(-1/par.sigma); % inverse of marginal utility of consumption
+func.w = @(K) par.z*(1-par.alpha)*K.^par.alpha*par.L^(-par.alpha); % wage
+func.r = @(K) par.z*par.alpha*K.^(par.alpha-1)*par.L^(1-par.alpha); % rental rate of capital
+func.K = @(r) par.L*(par.z*par.alpha/r).^(1/(1-par.alpha)); % capital stock necessary to yield return r
+func.Y = @(K) par.z*K.^par.alpha*par.L^(1-par.alpha); % output
+func.C = @(K) func.Y(K)-par.delta*K; % consumption
+func.U = @(c) (c.^(1-par.sigma))./(1-par.sigma); % Utility
+
+%%
+   
