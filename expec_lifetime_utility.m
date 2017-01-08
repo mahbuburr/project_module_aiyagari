9b4facfb59f96649e22ddb7ba834d0633166359b
Andrian Yambolov <a.yambolov@gmail.com>
2017-01-08 19:51:08 +0100

    some changes

--- expec_lifetime_utility.m
+++ expec_lifetime_utility.m
@@ -1,17 +1,11 @@
-function [ output_args ] = untitled5( sim_e, sim_k, K_demand, delta, r, w )
-
-
-
-end
-
 period = 2500;
 e = sim_e(period,:); %employment status 0 or 1
 e(e==1)=0; 
 e(e==2)=1;
-c_initial = (1+r(K_demand)-delta)*sim_k(period,:)-sim_k(period+1,:)+w(K_demand)*(1-tau)*(e) + mu*w(K_demand)*(1-e); %consumption for all individuals in the first period
-u_initial = (c_initial.^(1-sigma)-1)/(1-sigma); %utility from cons. in the first period
+c_initial = (1+r(K_demand)-delta)*sim_k(period,:)-sim_k(period+1,:)+w(K_demand)*(1-tau)*(e) + mu*w(K_demand)*(1-e); %consumption for all individuals 
+u_initial = (c_initial.^(1-sigma)-1)/(1-sigma); %utility from cons. 
 
-U = NaN(1,size(e,2));
+store.U = NaN(1,size(e,2));
 for individual = 1:size(e,2)
     U = zeros(2,grid_k_no); %expected life time utility
     dist=100;
@@ -21,7 +15,7 @@ for individual = 1:size(e,2)
         dist=max(max(abs(Unew-U)));
         U=Unew;
     end
-    U(individual) = interp1(grid_k,U(e(individual)+1,:),sim_k(period,individual),'linear','extrap');
+    store.U(individual) = interp1(grid_k,U(e(individual)+1,:),sim_k(period,individual),'linear','extrap');
 end
 
 
