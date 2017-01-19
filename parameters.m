2c121bddb01f1d3ddf27e11d0e0f4488eb679b21
Yannic Damm <yannicruda.damm@gmail.com>
2017-01-19 16:58:48 +0100

    Set calibration to Krussel-Smith

--- parameters.m
+++ parameters.m
@@ -2,17 +2,17 @@
 % consumers
 par.beta = 0.99; % discount factor
 par.sigma = 2; % risk aversion
-par.mu = 0.5; % replacement rate of unemployed
-par.k_min = 0.5; % borrowing constraint as share of capital stock
+par.mu = 0.15; % replacement rate of unemployed
+par.k_min = 0; % borrowing constraint as share of capital stock
 
 % firms (production function F(K,L)=z*K^alpha*L^(1-alpha)
 par.delta = 0.025; % depreciation rate
-par.alpha = 0.33; % output elasticity of capital
+par.alpha = 0.36; % output elasticity of capital
 par.z = 1; % productivity
 
 % transition probabilities
 par.L_target = 0.9;
-par.PI_UE = 0.3; % chance of getting employed
+par.PI_UE = 0.4; % chance of getting employed
 
 
 
